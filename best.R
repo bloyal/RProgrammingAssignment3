dde8bcdd04e7db2e64971a02568a25c38ad26c94
Brian Loyal <bloyal@gmail.com>
2014-05-30 15:40:29 -0500

    Completed best.R

--- best.R
+++ best.R
@@ -1,7 +1,5 @@
 best <- function(state, outcome) {
-        ## Read outcome data
-        #outcome <- readOutcomeData("outcome-of-care-measures.csv")
-        
+               
         ## Check that state and outcome are valid
         if (!isValidState(state)){
                 stop("invalid state")
@@ -10,8 +8,14 @@ best <- function(state, outcome) {
                 stop("invalid outcome")
         }
         
+        ## Read outcome data
+        outcomeData <- readOutcomeData("outcome-of-care-measures.csv")
+        
         ## Return hospital name in that state with lowest 30-day death
         ## rate
+        stateData <- getStateData(state, outcomeData)
+        stateData <- stateData[order(stateData[outcome],stateData["name"]) , ]
+        stateData[1,1]
 }
 
 readOutcomeData <- function(outcomeDataFile){
@@ -31,4 +35,16 @@ getListofStateCodes <- function (){
 isValidOutcome <- function(inputOutcome){
         validOutcomes <- c("heart attack", "heart failure", "pneumonia")
         any(validOutcomes==inputOutcome)     
+}
+
+getStateData <- function(state, outcomeData){
+        stateList<-split(outcomeData,outcomeData$State)
+        stateData<-stateList[[state]]
+        stateData<-stateData[,c("Hospital.Name",
+                     "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack",
+                     "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure",
+                     "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia")]
+        names(stateData)<-c("name","heart attack", "heart failure", "pneumonia")
+        stateData[,c(2,3,4)] <- sapply(stateData[,c(2,3,4)], as.numeric)
+        stateData
 }
